5fe17fe9f36e2981d1b95db1a6ef0629912a06bf
jiachenwong <jiachen5229@gmail.com>
2024-12-15 13:45:43 +0800

    added a module learn_decorator.py

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
+import learn_decorator
+
+
+##################################################################################
+#  This main.py serve as an entry of the entire program.
+#  All program written in this project will be calling from 
+#  this file.
+#
+###################################################################################
+
+
 
 def otherFunction():
    print("Other functions")
+
+def callingFunctionFromLearnDecorator():
+   learn_decorator.say_hello()
+   print(learn_decorator.add(2, 3))
+   learn_decorator.greet("Ali")
+   print(learn_decorator.greet.__name__)
+   print(learn_decorator.greet.__doc__)
 
 
 def main():
    print("Hello World")
    otherFunction()
+   callingFunctionFromLearnDecorator()
+   
    
    
 if __name__ == "__main__":
