c4da2708828fdc6a315f8dccf6015da7916e2e35
jiachenwong <jiachen5229@gmail.com>
2024-12-18 00:13:08 +0800

    Import function from module1.py.

--- package_1/__init__.py
+++ package_1/__init__.py
@@ -1,2 +1,8 @@
+# importing module from subpackage_1
 from package_1.subpackage_1.module1 import Function1, SubPackage1Class
 from package_1.subpackage_1.module2 import Function2
+
+
+
+# importing module from subpackage_2
+from package_1.subpackage_2.module1 import say_hello, add, greet
