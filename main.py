ea64c17e818422a622e43b8a81754cf25cba30d3
jiachenwong <jiachen5229@gmail.com>
2024-12-18 16:12:54 +0800

    Revert wrong resolution.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from package_1 import Function1, Function2, SubPackage1Class, say_hello, add, greet
+from package_1 import Function1, Function2, SubPackage1Class, LearnDecorator
 from Shape import Square, Rectangle, Triangle, Circle
 
 
@@ -15,17 +15,19 @@ from Shape import Square, Rectangle, Triangle, Circle
 def otherFunction():
    print("Other functions")
    callingFunctionFromShape()
-   callingFunctionFromLearnDecorator()
    Function2()
    Function1()
+   callingFunctionFromLearnDecorator()
    obj = SubPackage1Class(__name__)
 
 def callingFunctionFromLearnDecorator():
-   say_hello()
-   print(add(2, 3))
-   greet("Ali")
-   print(greet.__name__)
-   print(greet.__doc__)
+   obj = LearnDecorator(__name__)
+   obj.say_hello()
+   print(obj.add(2, 3))
+   print(obj.add(1, 5, 8))
+   obj.greet("Ali")
+   print(obj.greet.__name__)
+   print(obj.greet.__doc__)
    
 def callingFunctionFromShape():
    square = Square(10, 0, 6, 6)
